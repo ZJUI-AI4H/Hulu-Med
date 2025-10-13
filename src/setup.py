9c14f9c04333bf6509db2140c4ccca331dccb952
Eric Jiang <43131870+jiangsongtao@users.noreply.github.com>
2025-10-13 11:34:25 +0800

    Update

--- src/setup.py
+++ src/setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='hulu_med',  # 给你的包起一个名字
-    version='0.1.0',      # 版本号
-    packages=find_packages(), # 自动查找项目中的所有包（即包含__init__.py的目录）
+    name='hulu_med', 
+    version='0.1.0',     
+    packages=find_packages(), 
 )
